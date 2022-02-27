b851fcb10b7eaf4ccd7520a2a6b6048182904e9b
Piyush Kumar <20BCS9107@cuchd.in>
2022-02-27 11:40:22 +0530

    main class

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -2,7 +2,8 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
+
+public class HelloSpringApp {    
     public static void main(String[] args) {
 
         // load the spring configuration file
